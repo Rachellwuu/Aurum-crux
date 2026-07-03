6234b034039f95ea8bd3734e564e9a689260a757
Rachel <rachelwu321@gmail.com>
2026-07-04 01:06:10 +0800

    feat: calculate daily returns, generate signal with lookahead bias prevention, compute cumulative returns

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
+import pandas as pd
+pd.set_option('display.max_columns', None)
 
 ticker = yf.Ticker("AAPL")
 df = ticker.history(period="5y")
 
 df["MA50"] = df["Close"].rolling(window=50).mean()
 df["MA200"] = df["Close"].rolling(window=200).mean()
+df["Return"] = df["Close"].pct_change()
+df["Raw_signal"] = (df["MA50"] > df["MA200"]).astype(int)
+df["Signal"] = df["Raw_signal"].shift(1)
+df["Strategy_Return"] = df["Return"] * df["Signal"]
+print(df[["Close", "MA50", "MA200", "Return", "Raw_signal", "Signal"]].tail(10))
+print((1+ df["Return"]).cumprod())
+print((1+ df["Strategy_Return"]).cumprod())
 
 plt.figure(figsize=(12,6))
 plt.plot(df.index, df["Close"], label="AAPL Close", linewidth=1)
