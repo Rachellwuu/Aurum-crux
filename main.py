c343c9e0ecf1c37503459a01402176d03d03ba60
Rachel <rachelwu321@gmail.com>
2026-07-08 23:18:40 +0800

    feat: add transaction cost and slippage modeling to strategy returns

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import yfinance as yf
 import matplotlib.pyplot as plt
 import pandas as pd
 import indicators as ind
+import numpy as np
 pd.set_option('display.max_columns', None)
 
 ticker = yf.Ticker("AAPL")
@@ -18,12 +19,16 @@ df["RSI"] = ind.calculate_rsi(df["Close"], window=14)
 df["Return"] = df["Close"].pct_change()
 df["Raw_signal"] = ((df["MA50"] > df["MA200"]) & (df["VIX"] <25)).astype(int)
 df["Signal"] = df["Raw_signal"].shift(1)
-df["Strategy_Return"] = df["Return"] * df["Signal"]
+
 df["Position"] = df["Signal"]
 df["Entry"]= df["Signal"].diff() == 1
 df["Exit"]= df["Signal"].diff() == -1
-print(df[df["Exit"] == True][["Close", "Signal", "Entry", "Exit"]].head(10))
-
+df["Cost"] = np.zeros(len(df))
+df.loc[df["Entry"]|df["Exit"], "Cost"] = -0.001
+df["Strategy_Return"] = df["Return"] * df["Signal"] + df["Cost"]
+print(df["Close"].head(10))
+print((1 + df["Return"]).cumprod().iloc[-1])
+print((1 + df["Strategy_Return"]).cumprod().iloc[-1])
 
 plt.figure(figsize=(12,6))
 plt.plot(df.index, df["Close"], label="AAPL Close", linewidth=1)
